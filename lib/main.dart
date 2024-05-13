adf26c78ee69832bb28f8070b6c10777e47dcd81
Aakash Agarwal <aakash@instrik.com>
2024-05-14 04:38:40 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:audiofileplayer/audiofileplayer.dart';
+
+void main() {
+  runApp(Xylophone());
+}
+
+Widget keyMake(Color c, int i) {
+  Audio a = Audio.load('tones/note$i.wav');
+  return Expanded(
+    child: InkWell(
+      child: Container(
+        width: double.infinity,
+        color: c,
+      ),
+      onTap: () {
+        a.play();
+      },
+    ),
+  );
+}
+
+class Xylophone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Xylophone',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              keyMake(Colors.red, 1),
+              keyMake(Colors.yellowAccent, 2),
+              keyMake(Colors.orange, 3),
+              keyMake(Colors.blueAccent, 4),
+              keyMake(Colors.greenAccent, 5),
+              keyMake(Colors.redAccent, 6),
+              keyMake(Colors.orangeAccent, 7),
+              keyMake(Colors.yellowAccent, 8),
+              keyMake(Colors.lightBlueAccent, 9),
+              keyMake(Colors.redAccent, 10),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
